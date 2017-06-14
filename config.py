a109efb4cb55e83d093cd1a4b5bf32af4aa94abe
yellowbig <dahwang@126.com>
2017-06-14 16:30:44 +0800

    初步实现自动投票

---
+++ config.py
@@ -0,0 +1,1 @@
+SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
--- spider.py
+++ spider.py
@@ -3,42 +3,69 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from config import *
 import time
 
 
 class AutoVoter(object):
     def __init__(self):
-        option = webdriver.ChromeOptions()
-        option.add_argument('--incognito')
-        option.add_argument(
-            "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.24 Safari/537.36")
-        self.browser = webdriver.Chrome(chrome_options=option)
+        # self.browser = webdriver.PhantomJS(service_args=SERVICE_ARGS)
+        self.browser = webdriver.Chrome()
         self.wait = WebDriverWait(self.browser, 10)
+
         # it's 马麟
-        self.vote_button_xpath = '//*[@id="EconomyUser"]/div[4]/div[2]/div[2]/a[1]'
+        self._vote_button = '//*[@id="EconomyUser"]/div[4]/div[2]/div[2]/a[1]'
+        # '//*[@id="elect"]/div[6]/div/div[2]/div/input'
+        self._input_box = '#elect > div.alert > div > div.mobileInfo > div > input'
+        self._radio_box = '//*[@id="elect"]/div[6]/div/div[3]/div[2]/div[1]/span[1]/input'
+        self._option_box = "//option[@id='2']"
+        self._submit_button = '//*[@id="elect"]/div[6]/input'
 
     def vote(self):
+        print('正在投票')
         try:
             self.browser.get('http://i.feixin.10086.cn/activity/jnxcz/candidate/vote')
 
-            vote_button = self.browser.find_element_by_xpath(self.vote_button_xpath)
+            time.sleep(1)
+
+            vote_button = self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, self._vote_button))
+            )
             vote_button.click()
+
             time.sleep(1)
 
-            input_box = self.browser.find_element_by_xpath('//*[@id="elect"]/div[6]/div/div[2]/div/input')
-            input_box.send_keys('15022161502')
+            input_box = self.wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self._input_box))
+            )
+            input_box.send_keys('13702040007')
+
             time.sleep(1)
 
-            radio = self.browser.find_element_by_xpath('//*[@id="elect"]/div[6]/div/div[3]/div[2]/div[1]/span[1]/input')
+            radio = self.browser.find_element_by_xpath(self._radio_box)
             radio.click()
+
             time.sleep(1)
 
-            select_list = self.browser.find_element_by_xpath('//*[@id="provinceName"]')
-            select_list.find_element_by_xpath("//option[@id='2']").click()
+            option = self.wait.until(
+                EC.presence_of_element_located((By.XPATH, self._option_box))
+            )
+            option.click()
+
+            time.sleep(1)
+
+            submit_button = self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, self._submit_button))
+            )
+            submit_button.click()
+
             time.sleep(3)
 
-            submit_button = self.browser.find_element_by_xpath('//*[@id="elect"]/div[6]/input')
-            submit_button.click()
+            revote_button = self.wait.until(
+                EC.presence_of_element_located((By.XPATH, self._vote_button))
+            )
+            revote_button.click()
+            print('已投票')
 
         except TimeoutException:
             return self.vote()
