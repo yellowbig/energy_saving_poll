be97d6b86180152737e3e99053f7ce6f6d998e2b
yellowbig <dahwang@126.com>
2017-06-14 21:51:14 +0800

    又完善了一下

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
+MONGO_URL = 'localhost'
+MONGO_DB = 'tjmc'
+MONGO_TABLE = 'staff'
+
 SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
--- spider.py
+++ spider.py
@@ -5,11 +5,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from config import *
+from staff_mobile import *
 import time
+import pymongo
 
 
 class AutoVoter(object):
     def __init__(self):
+        # mongodb数据库
+        self.client = pymongo.MongoClient(MONGO_URL)
+        self.db = self.client[MONGO_DB]
+
         # self.browser = webdriver.PhantomJS(service_args=SERVICE_ARGS)
         self.browser = webdriver.Chrome()
         self.wait = WebDriverWait(self.browser, 10)
@@ -23,31 +29,33 @@ class AutoVoter(object):
         self._vote_success = '/html/body/div[4]/p'
         self._confirm_button = '/html/body/div[4]/div[7]/div/button'
 
-    def vote_main(self):
+    def vote_main(self, mobile_number):
         print('正在投票')
+
         try:
             self.browser.get('http://i.feixin.10086.cn/activity/jnxcz/candidate/vote')
 
             time.sleep(1)
 
-            self._do_vote()
+            self._do_vote(mobile_number)
 
             try:
                 self.close_floating()
             except NoSuchElementException:
                 print('投票后又返回了资料填写页面，重新填写中...')
-                self._do_vote(first_use=False)
+                self._do_vote(mobile_number, first_use=False)
                 self.close_floating()
 
             print('已投票')
 
             self.browser.quit()
 
+            time.sleep(1)
+
         except TimeoutException:
             self._do_vote()
 
-    def _do_vote(self, first_use=True):
-
+    def _do_vote(self, mobile_number, first_use=True):
         if first_use:
             vote_button = self.wait.until(
                 EC.element_to_be_clickable((By.XPATH, self._vote_button))
@@ -59,7 +67,7 @@ class AutoVoter(object):
             EC.presence_of_element_located((By.CSS_SELECTOR, self._input_box))
         )
         if first_use:
-            input_box.send_keys('13702040012')
+            input_box.send_keys(str(mobile_number))
 
         time.sleep(1)
 
@@ -93,7 +101,15 @@ class AutoVoter(object):
         self.browser.find_element_by_xpath(self._vote_success)
         self.browser.find_element_by_xpath(self._confirm_button).click()
 
+    @staticmethod
+    def get_batch_staff():
+        """
+        获得一批员工号码
+        """
+        return MOBILE_NUMBERS
+
 
 if __name__ == '__main__':
     av = AutoVoter()
-    av.vote_main()
+    for mobile_number in av.get_batch_staff():
+        av.vote_main(mobile_number)
